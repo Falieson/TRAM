723b5799970ec729276bb1fe06be702030ba9c7c
Florian Mettetal <florian.mettetal@flex.com>
2017-08-11 15:29:14 -0700

    :nut_and_bolt: ($contrib) TSLint config (prettier) and resolve all TS errors

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,17 +3,17 @@ import './App.css';
 
 import Login from './Login';
 
-const logo = require('./logo.svg');
+const logo = require('./logo.svg'); // tslint:disable-line no-var-requires
 
 class App extends React.Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+      <div className='App'>
+        <div className='App-header'>
+          <img src={logo} className='App-logo' alt='logo' />
           <h2>Welcome to React</h2>
         </div>
-        <div className="App-intro">
+        <div className='App-intro'>
           <Login />
         </div>
       </div>
--- client/src/Login.tsx
+++ client/src/Login.tsx
@@ -6,27 +6,27 @@ const Login = () => {
     e.preventDefault();
 
     asteroid.loginWithPassword({
-      username: 'admin',
       password: 'pass',
+      username: 'admin',
     })
-    .then((result) => {
+    .then(result => {
       // console.log({result})
     })
-    .catch((error) => {
+    .catch(error => {
       console.error(error.message);
     });
   };
   const form = () => (
-    <form onSubmit={(e) => handleLogin(e)}>
+    <form onSubmit={e => handleLogin(e)}>
       <div>
-        <input value="admin" type="text" name="username" placeholder="Username" />
+        <input value='admin' type='text' name='username' placeholder='Username' />
       </div>
       <div>
-        <input value="pass" type="password" name="password" placeholder="Password" />
+        <input value='pass' type='password' name='password' placeholder='Password' />
       </div>
       <div>(Username: admin | Password: pass)</div>
       <div>
-        <button type="submit">Login</button>
+        <button type='submit'>Login</button>
       </div>
     </form>
   );
