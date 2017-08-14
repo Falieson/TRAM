9b28b0dcfb5f10a594e6cf4824be076a49c72cd5
Florian Mettetal <florian.mettetal@flex.com>
2017-08-13 20:25:43 -0700

    :beetle: ($ui) Login module consoles response

--- client/src/routes/Login.tsx
+++ client/src/routes/Login.tsx
@@ -1,36 +1,75 @@
 import * as React from 'react';
 import asteroid from '../config/asteroid';
 
-const Login = () => {
-  const handleLogin = (e: React.FormEvent<HTMLFormElement>): void => {
+export default class Login extends React.Component<{}, {username: string, password: string}> {
+  constructor() {
+    super()
+
+    this.state = {
+      password: '',
+      username: '',
+    }
+
+    this.onUsernameInput = this.onUsernameInput.bind(this)
+    this.onPasswordInput = this.onPasswordInput.bind(this)
+    this.handleLogin = this.handleLogin.bind(this)
+  }
+
+  onUsernameInput(e: React.FormEvent<HTMLInputElement>) {
+    this.setState({
+      username: e.currentTarget.value
+    })
+  }
+
+  onPasswordInput(e: React.FormEvent<HTMLInputElement>) {
+    this.setState({
+      password: e.currentTarget.value
+    })
+  }
+
+  handleLogin(e: React.FormEvent<HTMLFormElement>) {
     e.preventDefault();
 
     asteroid.loginWithPassword({
-      password: 'pass',
-      username: 'admin',
+      password: this.state.password,
+      username: this.state.username,
     })
     .then(result => {
-      // console.log({result})
+      console.log({result}) // tslint:disable-line no-console
     })
     .catch(error => {
       console.error(error.message);
     });
   };
-  const form = () => (
-    <form onSubmit={e => handleLogin(e)}>
-      <div>
-        <input value='admin' type='text' name='username' placeholder='Username' />
-      </div>
-      <div>
-        <input value='pass' type='password' name='password' placeholder='Password' />
-      </div>
-      <div>(Username: admin | Password: pass)</div>
-      <div>
-        <button type='submit'>Login</button>
-      </div>
-    </form>
-  );
-  return <div>{form()}</div>;
-};
 
-export default Login;
+  render () {
+    return (
+      <div>
+        <form onSubmit={e => this.handleLogin(e)}>
+        <div>
+          <input
+            value={this.state.username}
+            onChange={this.onUsernameInput}
+            type='text'
+            name='username'
+            placeholder='Username'
+          />
+        </div>
+        <div>
+          <input
+            value={this.state.password}
+            onChange={this.onPasswordInput}
+            type='password'
+            name='password'
+            placeholder='Password'
+          />
+        </div>
+        <div>(Username: admin | Password: pass)</div>
+        <div>
+          <button type='submit'>Login</button>
+        </div>
+      </form>
+    </div>
+    )
+  }
+}
