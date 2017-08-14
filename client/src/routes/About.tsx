4ff8b7e0457d0a4c9224eccef52640e60af3dcbe
Florian Mettetal <florian.mettetal@flex.com>
2017-08-13 20:12:52 -0700

    :star2: ($arch) ReactRouter4 Installed and Demo'd

---
+++ client/src/routes/About.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react'
+
+const About = () => (
+  <div>
+    <h3>About Page</h3>
+    {/* <p>Meteor Connected: {props.connection.connected? 'true': 'false'}</p>
+    <p>Meteor userId: {props.userId}</p>
+    <p>Meteor loggingIn? {props.loggingIn}</p> */}
+  </div>
+)
+
+export default About
--- client/src/routes/App.tsx
+++ client/src/routes/App.tsx
@@ -1,21 +1,44 @@
 import * as React from 'react';
+import { BrowserRouter, Link, Route } from 'react-router-dom'
 import './App.css';
 
+import About from './About';
 import Login from './Login';
 
 const logo = require('./react_logo.svg'); // tslint:disable-line no-var-requires
 
 class App extends React.Component {
   render() {
+    const Navigation = () => (
+      <div className='App-navigation'>
+        <ul>
+          <li className='App-navigation--item'><Link to='/'>Home</Link></li>
+          <li className='App-navigation--item'><Link to='/about'>About</Link></li>
+        </ul>
+      </div>
+    )
+
     return (
       <div className='App'>
-        <div className='App-header'>
-          <img src={logo} className='App-logo' alt='logo' />
-          <h2>Welcome to React</h2>
-        </div>
-        <div className='App-intro'>
-          <Login />
-        </div>
+        <BrowserRouter>
+          <div>
+
+            <div className='App-header'>
+              <img src={logo} className='App-logo' alt='logo' />
+              <h2>Welcome to React</h2>
+
+              <Navigation />
+            </div>
+
+            <div className='App-intro'>
+              <div className='react-router'>
+                <Route exact={true} path='/' component={Login}/>
+                <Route exact={true} path='/about' component={About}/>
+              </div>
+            </div>   
+
+          </div>
+        </BrowserRouter>
       </div>
     );
   }
